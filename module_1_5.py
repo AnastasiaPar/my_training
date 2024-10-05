dafc9552098fc7d7627d1d02280a8a4693c84830
AnastasiaPar <anastasiaparfenteva6350@gmail.com>
2024-10-05 17:15:31 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+#immutable_var=(1,2,2,4,5, "apple")
+#print(immutable_var)
+#immutable_var[0]=1
+#print(immutable_var) кортеж не поддерживанте назначение символов
+mutable_list = (['apple' , 'coconut ' ],3)
+mutable_list[0][1]='orange'
+print(mutable_list)
